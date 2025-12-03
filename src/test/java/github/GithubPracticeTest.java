b96bd7d8676b9fc9d1699e01ea5e2ddd874e2ac4
ASHWINI JOLLE <ASHWINI JOLLE@ASHWINIJOLLE>
2025-12-03 15:16:28 +0530

    Frist commit

---
+++ src/test/java/github/GithubPracticeTest.java
@@ -0,0 +1,14 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubPracticeTest {
+
+	@Test
+	public void githubPracticeTest() {
+		Reporter.log("hi github", true);
+		String BROWSER = System.getProperty("browser");
+		System.out.println(BROWSER);
+	}
+}
